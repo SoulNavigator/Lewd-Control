59388770d9d24d0a33e6e4f1f5efb4fe9ed79177
SoulNavigator <igoryanigoryanich@gmail.com>
2020-06-05 02:12:01 +0200

    Made ButtonPanel

--- theproject/views.py
+++ theproject/views.py
@@ -3,21 +3,11 @@ import wx
 
 # pylint: disable=fixme, no-member
 class MainFrame(wx.Frame):
-    image_size = 800
     def __init__(self, parent, title):
-
         super().__init__(parent, title=title, style=wx.CAPTION | wx.CLOSE_BOX, size=wx.Size(self.image_size, self.image_size))
         self.init_ui()
         self.__make_binds()
-
-
-    # EVENTS:
-    def OnNSFWbuttonPress(self, event):
-        print("This picture is NSFW")
-
-    def OnSFWbuttonPress(self, event):
-        print("This picture is SFW")
-    
+   
     def OnMenuScanPress(self, event):
         wildcard = "pictures (*.jpeg,*.png,*.jpg)|*.jpeg;*.png;*.jpg"
         with wx.FileDialog(self, "Pick file", wildcard=wildcard) as file_dialog:
@@ -37,53 +27,19 @@ class MainFrame(wx.Frame):
 
         return toolbar
 
-    def open_image(self, path):
-        img = wx.Image(path, wx.BITMAP_TYPE_ANY)
-        ratio = self.__image_ratio(img)
-        img.Rescale(self.image_size*ratio, self.image_size)
-        self.SetSize(self.image_size*ratio, self.image_size+150)
-        
-        self.image_widget.SetBitmap(wx.BitmapFromImage(img))
-        img_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        img_sizer.Add(self.image_widget, 1, wx.ALL|wx.EXPAND, 5)
-        self.__pan_image.SetSizer(img_sizer)
-
-        #self.Refresh()
-
-    def __make_button_panel(self, sizer):
-        pan_buttons = wx.Panel(self)
-        sizer.Add(pan_buttons, 1, wx.ALL | wx.EXPAND, 5)
-
-        return pan_buttons
-
-    def __make_buttons(self, parent):
-        hbox = wx.BoxSizer(wx.HORIZONTAL)
-        btn_lewd = wx.Button(parent, label='OwO!')
-
-        btn_normal = wx.Button(parent, label='SFW')
-
-        hbox.Add(btn_normal,1,  wx.ALL |wx.EXPAND, 10)
-        hbox.Add(btn_lewd,2,  wx.ALL|wx.EXPAND,  10) 
-        parent.SetSizer(hbox)
-
-        return (btn_lewd, btn_normal)
-
     def __make_binds(self):
-        #Buttons
-        self.__btn_nsfw.Bind(wx.EVT_BUTTON, self.OnNSFWbuttonPress)
-        self.__btn_sfw.Bind(wx.EVT_BUTTON, self.OnSFWbuttonPress)
-        #Menu items
         self.Bind(wx.EVT_MENU, self.OnMenuScanPress, self.__i_scan)
         
     def init_ui(self):
         self.toolbar = self.__make_toolbar()
         mainbox = wx.BoxSizer(wx.VERTICAL)
-        #self.__pan_image = self.__make_image_panel(mainbox)
+
         self.pan_image = ImagePanel(self, mainbox)
-        self.__pan_buttons = self.__make_button_panel(mainbox)
+        self.pan_buttons = ButtonPanel(self, mainbox)
+        #self.__pan_buttons = self.__make_button_panel(mainbox)
         self.SetSizer(mainbox)
 
-        self.__btn_nsfw, self.__btn_sfw = self.__make_buttons(self.__pan_buttons)
+        #self.__btn_nsfw, self.__btn_sfw = self.__make_buttons(self.pan_buttons)
 
     
 
@@ -95,7 +51,6 @@ class ImagePanel(wx.Panel):
         sizer.Add(self, 5, wx.ALL | wx.EXPAND, 5)
 
         img = wx.EmptyImage(self.default_size, self.default_size)
-        #ratio = self.__image_ratio(img)
         img.Rescale(self.default_size, self.default_size)
         parent.SetSize(self.default_size, self.default_size+150)
 
@@ -122,8 +77,36 @@ class ImagePanel(wx.Panel):
         img_sizer = wx.BoxSizer(wx.HORIZONTAL)
         img_sizer.Add(self.image_widget, 1, wx.ALL|wx.EXPAND, 5)
         self.SetSizer(img_sizer)
-        
 
+class ButtonPanel(wx.Panel):
+    def __init__(self, parent, sizer):
+        self.parent = parent
+        super().__init__(parent)
+        sizer.Add(self, 1, wx.ALL | wx.EXPAND, 5)
+        self.init_buttons()
+
+    def init_buttons(self):
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.btn_yes = wx.Button(self, label='OwO!')
+        self.btn_no = wx.Button(self, label='SFW')
+
+        self.bind_buttons()
+
+        hbox.Add(self.btn_no,  1, wx.ALL|wx.EXPAND, 10)
+        hbox.Add(self.btn_yes, 2, wx.ALL|wx.EXPAND, 10) 
+        self.SetSizer(hbox)  
+
+    def bind_buttons(self):
+        self.btn_yes.Bind(wx.EVT_BUTTON, self.OnNSFWbuttonPress)
+        self.btn_no.Bind(wx.EVT_BUTTON, self.OnSFWbuttonPress)
+
+    def OnNSFWbuttonPress(self, event):
+        print("This picture is NSFW")
+
+    def OnSFWbuttonPress(self, event):
+        print("This picture is SFW")
+    
 
 app = wx.App(False)
 frame = MainFrame(None, "Lewd Control")
